bf5668c8c7a7020662624527658a57346927ffcd
Shih Han <swang05@mail.bbk.ac.uk>
2018-03-21 17:26:39 +0000

    returns docked bikes spec

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -8,6 +8,12 @@ describe DockingStation do
      expect(bike).to be_working
   end
 
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
   it { is_expected.to respond_to(:bike)  }
 
   it "raises an error if no bikes available" do
