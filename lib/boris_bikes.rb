d90283f9e15324922e7d37eb5b43de44c745dd26
Shih Han <swang05@mail.bbk.ac.uk>
2018-03-21 12:25:27 +0000

    10 bike working

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,10 +1,12 @@
+
 class DockingStation
   def release_bike
-
+    bike = Bike.new
   end
 end
+
 class Bike
   def working?
-
+    true
   end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,4 +1,9 @@
-require 'dockingstation'
+require 'boris_bikes'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike)  }
+
+  it "releases working bikes" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
