75e5aa78a5f425ba40128c1298707ff455d04088
Shih Han <swang05@mail.bbk.ac.uk>
2018-03-22 15:20:31 +0000

    change format

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,30 +1,29 @@
 
 class DockingStation
 
-attr_reader :collection
-attr_reader :bike
+  attr_reader :collection
+  attr_reader :bike
 
-def initialize
-  @collection = []
-end
+  def initialize
+    @collection = []
+  end
 
-def release_bike
-  fail 'No bikes available' unless @collection.length > 0
-  @bike
-  @collection.pop
-end
+  def release_bike
+    fail 'No bikes available' unless @collection.length > 0
+    @bike
+    @collection.pop
+  end
 
-def dock(bike)
-  fail "docking station full" if collection.length == 20
-  @bike = bike
-  @collection.push(@bike)
-
-end
+  def dock(bike)
+    fail "docking station full" if @collection.length == 20
+    @bike = bike
+    @collection.push(@bike)
+  end
 
 end
 
 class Bike
-attr_reader :dock_bike
+  attr_reader :dock_bike
   def working?
     true
   end
